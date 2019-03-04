6c1b46e1f11ea23a89011e8593a7489bc2fad2fd
misabiko <misabiko@gmail.com>
2019-03-04 01:15:42 -0500

    Added perlin noise + set consts in caps

--- Chunk.gd
+++ Chunk.gd
@@ -1,12 +1,21 @@
 extends Node
 
 const Block = preload("res://Block.tscn")
-const chunkSize = 16
-const worldHeight = 10
+onready var World = $".."
+const CHUNK_SIZE = 16
+const WORLD_HEIGHT = 10
+
+var xOffset
+var zOffset
 
 func _ready():
-	for x in range(chunkSize):
-		for z in range(chunkSize):
+	for x in range(CHUNK_SIZE):
+		for z in range(CHUNK_SIZE):
 			var block = Block.instance()
-			block.translate(Vector3(x, 0, z))
+			var pos = Vector2(x + xOffset * CHUNK_SIZE, z + zOffset * CHUNK_SIZE)
+			block.translate(Vector3(pos.x, floor(World.noise.get_noise_2dv(pos) * 4), pos.y))
 			add_child(block)
+
+func _init(x, z):
+	xOffset = x
+	zOffset = z
--- World.gd
+++ World.gd
@@ -1,10 +1,14 @@
 extends Node
 
-const Chunk = preload("res://Chunk.tscn")
+const Chunk = preload("res://Chunk.gd")
+var noise = OpenSimplexNoise.new()
 
 func _ready():
+	noise.seed = 43
+	noise.octaves = 4
+	noise.period = 20.0
+	noise.persistence = 0.8
+	
 	for x in range(3):
 		for y in range(3):
-			var chunk = Chunk.instance()
-			chunk.translate(Vector3(x, 0, y) * chunk.chunkSize)
-			add_child(chunk)
+			add_child(Chunk.new(x, y))
