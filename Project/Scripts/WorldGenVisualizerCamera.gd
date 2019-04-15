766a0000fb6eb268d8813459db42003e908a6ecc
misabiko <misabiko@gmail.com>
2019-04-14 22:28:17 -0400

    Tried to improve orbit cam, didn't much

--- Project/Scripts/WorldGenVisualizerCamera.gd
+++ Project/Scripts/WorldGenVisualizerCamera.gd
@@ -1,26 +1,29 @@
-extends Camera
+extends Spatial
 
 var anchor := Vector3(0, 60, 0)
 var rotating := false
-var offset := Vector3.BACK
+var offset := Vector3.BACK * 80
 
 func _process(delta):
-	translation = anchor + offset
+	#translation = anchor + offset
+	pass
 
 func _input(event):
 	if event is InputEventMouseButton:
 		match event.button_index:
 			BUTTON_MIDDLE:
 				rotating = event.pressed
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if rotating else Input.MOUSE_MODE_VISIBLE)
 			BUTTON_WHEEL_UP:
-				if event.pressed and offset.length() > 1:
-					offset = offset.normalized() * (offset.length() - 1)
+				if event.pressed and $Camera.translation.z > 5:
+					$Camera.translation.z -= 1
 			BUTTON_WHEEL_DOWN:
 				if event.pressed:
-					offset = offset.normalized() * (offset.length() + 1)
+					$Camera.translation.z += 1
 					
 	elif event is InputEventMouseMotion and rotating:
-		pass
+		rotate_y(-event.relative.x * 0.005)
+		$Camera.rotate(transform.basis.x, -event.relative.y * 0.005)
 	elif event is InputEventKey and event.pressed:
 		match event.scancode:
 			KEY_SPACE:
